117f465c87cdd93a50c30108a50e14d69688f063
moic-asu <35989382+moic-asu@users.noreply.github.com>
2018-02-10 00:08:32 +0200

    Add files via upload

---
+++ js/mySwiper.js
@@ -0,0 +1,10 @@
+<script>
+    var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+	
+
+  </script>
